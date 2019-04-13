4b2e5d4e46ebaf75aee01df959aa2bc5cc10875e
Chloe <chloe@Chloe.local>
2019-04-13 17:39:05 +0200

    Add axios library

---
+++ src/app/Posts.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+        <li v-for="post in posts" :key="post.id">
+            <span>{{ post.title }}</span>
+            <small>{{ post.body }}</small>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+    const axios = require('axios/index');
+
+    export default {
+        data() {
+            return {
+                posts: [],
+            };
+        },
+
+        name: 'Posts',
+
+        created() {
+            axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+                this.posts = response.data;
+            }).catch(error => console.log(error));
+        },
+    };
+</script>
--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import HelloWorld from './HelloWorld';
+import Posts from './Posts';
 
 new Vue({
  render: h => h(HelloWorld)
 }).$mount('#app');
+
+new Vue({
+ render: h => h(Posts)
+}).$mount('#app2');
